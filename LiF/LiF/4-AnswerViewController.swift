e0de1fff9fc6d9a25f7bd8e5ba209fec9b768de7
YANG FENG LI <yfl@10-150-45-27.dhcp-m.deakin.edu.au>
2018-05-08 17:13:57 +1000

    Week 8 status

---
+++ LiF/LiF/4-AnswerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  4-AnswerViewController.swift
+//  LiF
+//
+//  Created by YANG FENG LI on 8/5/18.
+//  Copyright © 2018 YANG FENG LI. All rights reserved.
+//
+
+import UIKit
+
+class __AnswerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LiF/LiF/ViewController.swift
+++ LiF/LiF/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var musicPlayer: AVAudioPlayer!
+    
+    func playMusicWith(fileName: String, fileExtension: String) -> Void {
+        let musicURL: URL!
+        musicURL = Bundle.main.url(forResource: fileName, withExtension: fileExtension)
+        
+        if musicURL == nil
+        {
+            print("requested song cannot be played")
+        }
+        else
+        {
+            do
+            {
+                musicPlayer = try AVAudioPlayer.init(contentsOf: musicURL)
+                musicPlayer.prepareToPlay()
+                musicPlayer.play()
+            }
+            catch
+            {
+                print(error)
+            }
+        }
+    }
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    @IBAction func playMusic(_ sender: UIButton) {
+        pauseButton.isHidden = false
+        playButton.isHidden = true
+        
+        playMusicWith(fileName: "joeshisha_summer", fileExtension: "mp3")
+        
+    }
+    @IBAction func pauseMusic(_ sender: UIButton) {
+        pauseButton.isHidden = true
+        playButton.isHidden = false
+        musicPlayer.pause()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pauseButton.isHidden = true
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
